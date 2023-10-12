339a5ce20e39ec65299915b9d8a4d2085d53a5bd
Rabie Jebbor <rabie.jebbor@facturee.de>
2023-10-12 20:08:41 +0200

    build

--- assets/index3-legacy-2e261143.js
+++ assets/index3-legacy-2e261143.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-9538db83.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
+System.register(["./index-legacy-2b8939cf.js"],(function(e,t){"use strict";var r;return{setters:[t=>{r=t.G,e("GESTURE_CONTROLLER",t.G)}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/md.transition-legacy-0df62de0.js
+++ assets/md.transition-legacy-0df62de0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-9538db83.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
+System.register(["./index-legacy-2b8939cf.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.m}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
