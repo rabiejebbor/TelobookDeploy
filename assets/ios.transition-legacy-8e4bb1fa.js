c0771c34785d80dbb4238ee11661b0ec68a25ba5
Rabie Jebbor <rabie.jebbor@facturee.de>
2024-02-05 08:32:41 +0100

    deploy

--- assets/ios.transition-legacy-8e4bb1fa.js
+++ assets/ios.transition-legacy-8e4bb1fa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-7b53e173.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-989a6475.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
