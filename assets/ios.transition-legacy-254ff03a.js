d2da11654798476f3573300cfea753cafce0887c
Rabie Jebbor <rabiejebbor@gmail.com>
2024-02-25 14:41:13 +0100

    deploy

--- assets/ios.transition-legacy-254ff03a.js
+++ assets/ios.transition-legacy-254ff03a.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-d7e9682e.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-3f62c2db.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-1eb9712f.js
+++ assets/md.transition-legacy-1eb9712f.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-d7e9682e.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-3f62c2db.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
