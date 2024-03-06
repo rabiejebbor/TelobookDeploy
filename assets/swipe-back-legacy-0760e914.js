cc4a9ce9f269e39e2565c700a645d1805de924ef
Rabie Jebbor <rabiejeboor@gmail.com>
2024-03-06 22:01:29 +0100

    fix deploy

--- assets/swipe-back-legacy-0760e914.js
+++ assets/swipe-back-legacy-0760e914.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-f1a8b493.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-de4c21a6.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
