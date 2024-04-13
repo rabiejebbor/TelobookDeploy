76ff54c3b3d6601f310bf7784f8083d3e32405d1
Rabie Jebbor <rabie.jebbor@facturee.de>
2024-04-13 18:59:39 +0200

    deploy

--- assets/status-tap-legacy-4d8eb3ce.js
+++ assets/status-tap-legacy-4d8eb3ce.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c6af01c0.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-8563014c.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-2743b623.js
+++ assets/swipe-back-legacy-2743b623.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-c6af01c0.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-8563014c.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
