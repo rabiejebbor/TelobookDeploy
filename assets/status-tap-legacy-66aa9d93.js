2376abea85648775ea17c4ce8127f2f690ea4355
Rabie Jebbor <rabie.jebbor@facturee.de>
2024-04-14 13:41:37 +0200

    deploy

--- assets/status-tap-legacy-66aa9d93.js
+++ assets/status-tap-legacy-66aa9d93.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1b5d3e91.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-dddd8212.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
