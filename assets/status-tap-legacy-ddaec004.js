d9612179016adece745b08481dee79094a3ef5d6
Rabie Jebbor <rabiejebbor@gmail.com>
2024-07-08 22:12:30 +0200

    deploy 0.2.13

--- assets/status-tap-legacy-ddaec004.js
+++ assets/status-tap-legacy-ddaec004.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-59662ebf.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-e9f70089.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
