a8ade6ee2e013d694da3c1b59d68d328cac6c4ac
Rabie Jebbor <rabiejebbor@gmail.com>
2024-07-21 10:59:03 +0200

    deploy 0.2.16

--- assets/md.transition-legacy-3a968fe0.js
+++ assets/md.transition-legacy-3a968fe0.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-d3f8190a.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-b7eb3cb8.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-c47c7bc3.js
+++ assets/swipe-back-legacy-c47c7bc3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-d3f8190a.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-b7eb3cb8.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
