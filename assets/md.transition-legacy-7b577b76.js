e123193a80974885ed1af8e16336873720383b6c
Rabie Jebbor <rabiejebbor@gmail.com>
2024-10-06 22:45:10 +0200

    Deploy 0.5.0

--- assets/md.transition-legacy-7b577b76.js
+++ assets/md.transition-legacy-7b577b76.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-35e77903.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-ffe69cb7.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
