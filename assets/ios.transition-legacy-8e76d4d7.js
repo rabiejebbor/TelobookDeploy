10bed6022a50c6c0d37c19a53aa845c3d272a583
Rabie Jebbor <rabiejebbor@gmail.com>
2025-05-29 18:58:49 +0200

    Deploy 0.14.4

--- assets/ios.transition-legacy-8e76d4d7.js
+++ assets/ios.transition-legacy-8e76d4d7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a78db4b2.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-4854c617.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/status-tap-legacy-c74357e7.js
+++ assets/status-tap-legacy-c74357e7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a78db4b2.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-4854c617.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.b,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
