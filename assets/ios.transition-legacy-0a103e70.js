f506ba8281efa258c2b721a40f88517b2bd48cb2
Rabie Jebbor <rabiejebbor@gmail.com>
2025-07-24 22:16:09 +0200

    v0.15.0

--- assets/ios.transition-legacy-0a103e70.js
+++ assets/ios.transition-legacy-0a103e70.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bb9aa6a1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-764bbca9.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-ccf9ecfa.js
+++ assets/md.transition-legacy-ccf9ecfa.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-bb9aa6a1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-764bbca9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
